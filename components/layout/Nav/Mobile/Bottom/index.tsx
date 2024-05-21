2d5b78c68c0641fafb99a53e33709ad6ed43f6ad
Joe Toledano <joseph.a.toledano@gmail.com>
2024-05-20 23:49:41 -0700

    fixing typing issue

--- components/layout/Nav/Mobile/Bottom/index.tsx
+++ components/layout/Nav/Mobile/Bottom/index.tsx
@@ -1,9 +1,18 @@
+import { Cast as CastType } from "@/types"
+
 import MobileFiltering from "@/components/layout/Nav/Mobile/Bottom/Filters"
 import MobileRankings from "@/components/layout/Nav/Mobile/Bottom/Rankings"
 import MobileSearch from "@/components/layout/Nav/Mobile/Bottom/Search"
 import MobileSorting from "@/components/layout/Nav/Mobile/Bottom/Sort"
 
-const BottomMobileNav = ({ initialCasts, filteredCasts }) => {
+interface BottomMobileNavProps {
+  initialCasts: CastType[]
+  filteredCasts: CastType[]
+}
+const BottomMobileNav = ({
+  initialCasts,
+  filteredCasts,
+}: BottomMobileNavProps) => {
   return (
     <div className="bg-background fixed bottom-0 z-40 flex h-20 w-full flex-row items-center justify-around gap-x-10 overflow-hidden border-t px-0">
       <MobileSearch />
