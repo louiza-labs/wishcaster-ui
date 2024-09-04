84aeea57c0b797c42ed976974dd7cad42681db31
Joe Toledano <joseph.a.toledano@gmail.com>
2024-09-03 18:27:20 -0700

    fixing disabled state

--- components/filters/User/new/index.tsx
+++ components/filters/User/new/index.tsx
@@ -106,6 +106,7 @@ const UserFilters = () => {
             value={filterIsSelected("following")}
             text={"Following"}
             id={"following"}
+            isDisabled={!isAuthenticated}
           />
 
           <InteractionsCheckbox
